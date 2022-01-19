392d9db334a0c5fe2fd623bbe62f98d343c810b3
ChleaBourtoule <chlea.bourtoule@gmail.com>
2022-01-19 22:57:27 +0100

    user skeleton routes done

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const express = require("express");
+const { setupRoutes } = require("./controllers");
+const cors = require("cors");
 const app = express();
+import "dotenv/config";
 
-const connection = require ("./db-config");
-const Joi = require('joi');
+const connection = require("./db-config");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const port = 8000;
 
 app.use(express.json());
-
-// GET ----------------------------
-
-app.get("/coucou", (req, res) => {res.status(200).send("hibou")});
-
+app.app // GET ----------------------------
+  .get("/coucou", (req, res) => {
+    res.status(200).send("hibou");
+  });
 
 // POST ---------------------------
 
-
 // PUT ---------------------------
-
-
 
 // LISTEN -------------------------
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+  console.log(`Server listening on port ${port}`);
+});
